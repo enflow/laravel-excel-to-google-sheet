4c37e68461455a942c5316738a74b14611bbe1c1
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2023-07-14 15:30:47 +0200

    Use lazy collection for writing values, fix multiline CSV columns

--- src/GoogleSheetPusher.php
+++ src/GoogleSheetPusher.php
@@ -3,15 +3,16 @@
 namespace Enflow\LaravelExcelToGoogleSheet;
 
 use Google\Exception as GoogleException;
-use Illuminate\Support\Collection;
+use Illuminate\Support\LazyCollection;
 use Maatwebsite\Excel\Excel;
-use Maatwebsite\Excel\Facades\Excel as ExcelFacade;
+use Maatwebsite\Excel\Writer;
 
 class GoogleSheetPusher
 {
     public function __construct(
         protected GoogleSheetService $googleSheetService,
-    ) {
+    )
+    {
 
     }
 
@@ -21,7 +22,8 @@ class GoogleSheetPusher
             $export->prepare();
         }
 
-        $rawExport = ExcelFacade::raw($export, Excel::CSV);
+        $writer = app(Writer::class)->export($export, Excel::CSV);
+        $handle = fopen($writer->getLocalPath(), 'r');
 
         $sheetName = $export->title();
 
@@ -29,17 +31,22 @@ class GoogleSheetPusher
         $this->googleSheetService->clearSheet($export->googleSpreadsheetId(), $sheetName);
 
         try {
-            collect(explode("\n", $rawExport))
-                ->map(fn (string $row) => str_getcsv($row))
-                ->chunk(5000)
-                ->each(function (Collection $chunk) use ($export, $sheetName) {
-                    // Send the data to the Google Sheet.
-                    $this->googleSheetService->insert(
-                        spreadsheetId: $export->googleSpreadsheetId(),
-                        range: $sheetName,
-                        values: $chunk->values()->all(),
-                    );
-                });
+            LazyCollection::make(function () use ($handle) {
+                while ($line = fgetcsv($handle)) {
+                    yield $line;
+                }
+            })->chunk(5000)->each(function (LazyCollection $chunk) use ($export, $sheetName) {
+                // Send the data to the Google Sheet.
+                $this->googleSheetService->insert(
+                    spreadsheetId: $export->googleSpreadsheetId(),
+                    range: $sheetName,
+                    values: $chunk->values()->all(),
+                );
+            });
+
+            if (is_resource($handle)) {
+                fclose($handle);
+            }
         } catch (GoogleException $e) {
             // Clear the complete sheet if (a chunk) fails.
             // We don't want to end up with a half-filled sheet.
