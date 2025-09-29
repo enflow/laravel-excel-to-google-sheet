7aad4cf4b0ffe72c4ac3f12b606d19fd5c9c95cb
mbardelmeijer <1583095+mbardelmeijer@users.noreply.github.com>
2025-09-29 10:42:36 +0000

    Fix styling

--- src/PusherFactory.php
+++ src/PusherFactory.php
@@ -2,9 +2,6 @@
 
 namespace Enflow\LaravelExcelExporter;
 
-use Enflow\LaravelExcelExporter\Exporters\GoogleSheet\ExportableToGoogleSheet;
-use Enflow\LaravelExcelExporter\Exporters\GoogleSheet\GoogleSheetPusher;
-use Exception;
 use Illuminate\Support\Collection;
 
 class PusherFactory
@@ -14,8 +11,8 @@ class PusherFactory
     public static function make(Exportable $export): Collection
     {
         return collect(static::$exporters)
-            ->filter(fn(string $pusher, string $exporter) => $export instanceof $exporter)
-            ->map(fn($pusher) => app($pusher, ['export' => $export]))
+            ->filter(fn (string $pusher, string $exporter) => $export instanceof $exporter)
+            ->map(fn ($pusher) => app($pusher, ['export' => $export]))
             ->values();
     }
 
